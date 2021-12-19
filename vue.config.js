90cb64b9f013960fb85356ba82756d27d38d9398
Hwang Chaeeun <ch5549@naver.com>
2021-12-19 19:46:56 +0900

    fix error

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             'api/': {
-                target: 'http://127.0.0.1:3000',
+                target: 'http://127.0.0.1:8000',
                 changeOrigin: true,
                 logLevel: 'debug',
                 pathRewrite: {'api/': ''}
